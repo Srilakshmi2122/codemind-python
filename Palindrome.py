d99fa9446b3dedad64294ceffb088be0c27b3101
Srilakshmi2122 <119720694+Srilakshmi2122@users.noreply.github.com>
2023-09-07 14:51:34 +0530

    def palin(n):     s=0     q=n     while n!=0:         r=n%10         s=s*10+r         n=n//10     if q==s:         print(True)     else:         print(False) num=int(input()) palin(num)

--- Palindrome.py
+++ Palindrome.py
@@ -1,11 +1,13 @@
-n=int(input())
-s=0
-q=n
-while q!=0:
-    r=q%10
-    q=q//10
-    s=s*10+r
-if n==s:
-    print("True")
-else:
-    print("False")
+def palin(n):
+    s=0
+    q=n
+    while n!=0:
+        r=n%10
+        s=s*10+r
+        n=n//10
+    if q==s:
+        print(True)
+    else:
+        print(False)
+num=int(input())
+palin(num)
